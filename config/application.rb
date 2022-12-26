a17d177a677e95f2b8dd50c111026e64c119a1d3
micchy466 <48757027+micchy466@users.noreply.github.com>
2022-12-26 22:35:12 +0900

    🔧 ロケールを日本語に設定

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module HotwireSample
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
   end
 end
