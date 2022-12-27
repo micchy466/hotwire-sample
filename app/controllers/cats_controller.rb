a708d332f2982d4d97777ee206c625459dae2d77
micchy466 <48757027+micchy466@users.noreply.github.com>
2022-12-27 11:09:36 +0900

    ⚡️ Turbo StreamsでFlashを表示

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -35,7 +35,7 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "ねこを更新しました。"
+      @clog.info @cat.inspect
     else
       render :edit, status: :unprocessable_entity
     end
